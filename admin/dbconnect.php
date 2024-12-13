d30945f4bc60c6c995cd932cf7c2a6c322dc4f23
Faithan <khemark.ocariza@gmail.com>
2024-12-13 15:11:57 +0800

    timezone updated

--- admin/dbconnect.php
+++ admin/dbconnect.php
@@ -1,3 +1,8 @@
 <?php
     $conn = mysqli_connect ('localhost','root','','royale');
 ?>
+
+<?php
+// Set the default timezone
+date_default_timezone_set('Asia/Manila'); // Adjust based on your server's location
+?>
